ef7bfccc07ecd96cb7913f3dbbd70fa275bfff28
Łukasz <draganl@student.mi.pw.edu.pl>
2016-11-21 20:40:51 +0100

    uproszczenie

--- main.pl
+++ main.pl
@@ -1,57 +1,34 @@
-bird(X) :-
-	retractall(known(Y,A,V)),
-	birdA(X).
+cloud(X) :-
+	retractall(known(Y,A)),
+	cloudBuf(X).
 
-birdA(laysan_albatross):-
-	family(albatross),
-	color(white).
-birdA(black_footed_albatross):-
-	family(albatross),
-	color(dark).
-birdA(whistling_swan) :-
-	family(swan),
-	voice(muffled_musical_whistle).
-birdA(trumpeter_swan) :-
-	family(swan),
-	voice(loud_trumpeting).
-
-order(tubenose) :-
-	nostrils(external_tubular),
-	live(at_sea),
-	bill(hooked).
-order(waterfowl) :-
-	feet(webbed),
-	bill(flat).
-family(albatross) :-
-	order(tubenose),
-	size(large),
-	wings(long_narrow).
-family(swan) :-
-	order(waterfowl),
-	neck(long),
-	color(white),
-	flight(ponderous).
-
-:- dynamic known/3.
+:- dynamic known/2.
 	
-ask(A, V):-
-	known(yes, A, V), % succeed if true
+ask(A):-
+	known(yes, A), % succeed if true
 	!. % stop looking
-ask(A, V):-
-	known(_, A, V), % fail if false
+ask(A):-
+	known(_, A), % fail if false
 	!,
 	fail.
-ask(A, V):-
-	write(A:V), % ask user
+ask(A):-
+	write(A), % ask user
 	write('? : '),
 	read(Y), % get the answer
-	asserta(known(Y, A, V)), % remember it
+	asserta(known(Y, A)), % remember it
 	Y == yes. % succeed or fail
+
 	
-nostrils(X):- ask(nostrils, X).
-live(X):- ask(live, X).
-bill(X):- ask(bill, X).
-size(X):- ask(size, X).
-wings(X):- ask(wings, X).
-color(X):- ask(color, X).
-feet(X):- ask(feet, X).
+	
+cloudBuf(laysan_albatross):-
+	ask(albatross),
+	ask(white).
+cloudBuf(black_footed_albatross):-
+	ask(albatross),
+	ask(dark).
+cloudBuf(whistling_swan) :-
+	ask(swan),
+	ask(muffled_musical_whistle).
+cloudBuf(trumpeter_swan) :-
+	ask(swan),
+	ask(loud_trumpeting).
