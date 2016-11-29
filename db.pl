ff4d6fe8b4c9e98a2c6cfdb0a62ad8897250d85d
Łukasz <draganl@student.mi.pw.edu.pl>
2016-11-29 17:51:20 +0100

    wszystkie chmury

--- db.pl
+++ db.pl
@@ -1,9 +1,9 @@
 cloudBuf(nimbostratus):-
-	ask(deszcz),
+	ask(pada_deszcz),
 	ask(zaslania_cale_niebo).
 	
 cloudBuf(cumulonimbus):-
-	ask(deszcz).
+	ask(pada_deszcz).
 
 cloudBuf(stratus) :-
 	ask(jest_nisko),
@@ -14,7 +14,18 @@ cloudBuf(stratocumulus) :-
 	ask(jest_nisko),
 	ask(zaslania_cale_niebo).
 	
-cloudBuf(cumulus) :-
-	ask(jest_nisko).
-cloudBuf(stratus) :-ask(zaslania_cale_niebo),
-!.
+cloudBuf(altostratus) :-
+	ask(jest_gladka),
+	ask(pokrywa_wiekszosc_nieba).
+	
+cloudBuf(cirrostratus) :-
+	ask(jest_gladka),
+	ask(jest_zwarta).
+	
+cloudBuf(cirrus) :-
+	ask(jest_gladka).
+	
+cloudBuf(altocumulus) :-
+	ask(pokrywa_wiekszosc_nieba).
+	
+cloudBuf(cirrocumulus).
