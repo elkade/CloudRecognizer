dc6eaed15c632a44b2d55fc9ed365499f54e4dfa
Łukasz <draganl@student.mi.pw.edu.pl>
2017-01-04 21:21:46 +0100

    lisp działające budowanie pozytywnej gałęzi

--- main.lisp
+++ main.lisp
@@ -10,7 +10,32 @@
 (princ data)
 ;;;(print (car (car data)))
 
-(setq tree '("pada_deszcz" '("zaslania_cale" '(Nil "nimbostratus") '(Nil "cumulonimbus")) Nil))
+(defun node-name (node) (car node))
+(defun node-pos (node) (car (cdr node)))
+(defun node-neg (node) (car (cdr (cdr node))))
+
+(defun add_elem (_node _name _pos _neg)
+	(cond
+		((null _node) (cond
+				((null (car _pos)) (list Nil _name))
+				(T (list (car _pos) (add_elem Nil _name (remove (car _pos) _pos) _neg) Nil))
+			))
+		((null (car _node)) _node)
+		((null (find _pos (car _node)))
+			  (list (node-name _node) (add_elem (car (cdr _node)) _name (remove (car _node) _pos ) _neg) Nil))
+		(T _node)
+	)
+)
+
+(defun make-tree (_tree _data)
+(cond ((null _data) _tree)
+	(T (make-tree (add_elem _tree (node-name (car data)) (node-pos (car data)) (node-neg (car data)))(cdr _data)))
+	)
+) 
+
+(trace add_elem)
+
+(setq tree (make-tree Nil data))
 
 (defun test (node)
 	(cond
@@ -19,8 +44,8 @@
 		(T (print (car node)) (setq ans (read)) 
 			
 			(cond
-			((string-equal "Y" ans) (test (eval(car (cdr node)))) )
-			(T (test (eval(car (cdr (cdr node))))))
+			((string-equal "Y" ans) (test (car (cdr node))) )
+			(T (test (car (cdr (cdr node)))))
 		))
 	)
 )
@@ -32,4 +57,4 @@
 	(str "db.lisp" :direction :output :if-exists :append)
 	(format str "~%(setq data (append data (list ~%~a~%)))" suffix))
 	)
-(save_rule)
+;(save_rule)
